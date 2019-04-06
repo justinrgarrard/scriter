07c8477339f0f5817c97728a86ec9d2feb757f37
jgarrard <jgarrard@cableone.net>
2019-04-06 11:54:07 -0400

    Initial commit; minimum viable product web scraper complete.

---
+++ README.md
@@ -0,0 +1,31 @@
+# README
+
+Scriter is a web application that scrapes job posting data from various
+websites and visualizes the outputs on a server.
+
+
+### Architecture
+
+For prototyping purposes, the following three components run on the
+same server.
+
+* Weekly Web Scraper Job to Gather Data (Python + cron)
+
+* DB to Store Data (Postgres)
+
+* Web Server to Display Data (Apache)
+
+
+### Development Status
+
+**Data Load**
+
+| Status  | Task |
+|---|---|
+| Done | Basic Web Scraper |
+| TBD | Data Cleaning Process |
+| TBD | Database Setup |
+| TBD | Database Load Process |
+| TBD | Cron Web Scrape |
+
+
---
+++ web_scrape.py
@@ -0,0 +1,36 @@
+#!/bin/python
+"""
+A script that scrapes a specified website for job posting data.
+
+scrapy runspider web_scrape.py -o links.json -s CLOSESPIDER_PAGECOUNT=100
+"""
+
+import scrapy
+
+
+class IndeedJobPostSpider(scrapy.Spider):
+    """
+    A Scrapy Spider designed to scrape Indeed.com.
+    """
+    name = 'indeed'
+    title = 'software+engineer'
+    start_urls = ['https://www.indeed.com/jobs?q=' + title]
+
+    def parse(self, response):
+        # Pull all links related to job postings
+        for posting_link in response.css('a::attr(href)').re(r'/rc.*|/company.*|/pagead.*'):
+            # Follow each link to collect job posting data
+            yield response.follow(posting_link, self.parse_posting_link)
+
+        # Follow to the next page of results indefinitely
+        next_page = response.css('a::attr(href)').re(r'.*start=.*')
+        if next_page is not None:
+            for page in next_page:
+                yield response.follow(page, self.parse)
+
+    def parse_posting_link(self, response):
+        # Return the URL and the contents of the job posting
+        yield {
+            'hyperlink': response.request.url,
+            'post_data': response.css('div *::text').extract()
+        }
