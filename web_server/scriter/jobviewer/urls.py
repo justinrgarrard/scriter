1d5da9ff7c07249cf599dd5a387567a37961e491
roy <justinrgarrard@gmail.com>
2019-08-31 17:16:18 -0400

    Experimenting with views III

--- web_server/scriter/jobviewer/urls.py
+++ web_server/scriter/jobviewer/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('json-example/', views.job_json, name='json_example'),
-    path('json-example/data/', views.chart_data, name='chart_data'),
+    path('job-json/', views.job_json, name='json_example'),
+    path('job-json/data/', views.chart_data, name='chart_data'),
 ]
