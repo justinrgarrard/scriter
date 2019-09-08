732a1de3bd72326a4756eaee69490011f50b3788
roy <justinrgarrard@gmail.com>
2019-09-08 08:43:40 -0400

    Set basic structure of test suite

---
+++ test/test.py
@@ -0,0 +1,25 @@
+"""
+A test suite script for assessing basic functionality and use cases.
+"""
+
+import unittest
+
+
+class TestWebScrape(unittest.TestCase):
+    pass
+
+
+class TestDataLoad(unittest.TestCase):
+    pass
+
+
+class TestModelBuild(unittest.TestCase):
+    pass
+
+
+class TestWebServer(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
