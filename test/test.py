a90ba4bd57f76d3dca79c9f147ce2d09c4f5efd5
roy <justinrgarrard@gmail.com>
2019-09-08 09:52:05 -0400

    Second test

--- test/test.py
+++ test/test.py
@@ -1,85 +1,113 @@
 """
 A test suite script for assessing basic functionality and use cases.
+
+Should be run after setup is complete and as the 'scriter' user.
 """
 
 import os
 import shutil
 import unittest
 import subprocess
+import json
+import logging
+import argparse
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+
+LOG_FORMAT = '%(asctime)s: %(filename)s [%(funcName)s]- %(message)s'
+logging.basicConfig(format=LOG_FORMAT, level=logging.DEBUG)
+LOGGER = logging.getLogger()
+TECH_LISTING = 'tech.json'
+DB_USER = os.environ['DB_USER']
+DB_PASS = os.environ['DB_PASS']
+INGEST_DB_CONN_STR = 'postgresql://{0}:{1}@localhost/scriter_ingest'.format(DB_USER, DB_PASS)
+MODEL_DB_CONN_STR = 'postgresql://{0}:{1}@localhost/scriter_web'.format(DB_USER, DB_PASS)
 
 
 class TestWebScrape(unittest.TestCase):
     def test_run(self):
         """
-        Basic Completion
+        Basic Functional Test
         :return:
         """
-        cmd = "scrapy runspider web_scrape.py -o test.csv -s CLOSESPIDER_PAGECOUNT=50 -a job_title='software+engineer'"
-        output = subprocess.check_output(cmd, cwd='../web_scraper', shell=True)
-        self.assertTrue(os.path.exists('../web_scraper/test.csv'))
-        os.remove('../web_scraper/test.csv')
+        # Command to run
+        cmd = "scrapy runspider web_scrape.py -o test_links.csv -s CLOSESPIDER_PAGECOUNT=50 -a job_title='software+engineer'  > test_log.txt 2>&1"
+        target_dir = '../web_scraper'
 
-    def test_scrape_count(self):
-        """
-        CLOSESPIDER_PAGECOUNT ~ Output Count
-        :return:
-        """
-        pass
+        # Execute and store any output
+        output = subprocess.check_output(cmd, cwd=target_dir, shell=True)
+
+        # Assert results match expectations
+        self.assertTrue(os.path.exists('../web_scraper/test_links.csv'))
+        self.assertTrue(os.path.exists('../web_scraper/test_log.txt'))
+        ## TODO: CLOSESPIDER_PAGECOUNT ~ Output Count
+
+        # Cleanup
+        os.remove('../web_scraper/test_links.csv')
+        os.remove('../web_scraper/test_log.txt')
 
 
 class TestDataLoad(unittest.TestCase):
     def test_run(self):
         """
-        Basic Completion
+        Basic Functional Test
         :return:
         """
-        pass
+        # Command to run
+        cmd = "python data_load.py test_links.csv test_title  > test_log.txt 2>&1"
+        target_dir = '../web_scraper'
 
-    def test_scrape_count(self):
-        """
-        Input Shape ~ Output Shape
-        :return:
-        """
-        pass
+        # Prior setup
+        tbl_drop_cmd = "psql -d scriter_ingest -c 'DROP TABLE IF EXISTS test_title'"
+        output = subprocess.check_output(tbl_drop_cmd, cwd=target_dir, shell=True)
+        shutil.copy('test_links.csv', '../web_server')
 
-    def test_scrape_count_duplicates(self):
-        """
-        Duplicate Detection
-        :return:
-        """
-        pass
+        # Execute and store any output
+        output = subprocess.check_output(cmd, cwd=target_dir, shell=True)
+
+        # Assert results match expectations
+        self.assertTrue(os.path.exists('../web_scraper/test_links.csv'))
+        self.assertTrue(os.path.exists('../web_scraper/test_log.txt'))
+        ## TODO: Input Shape ~ Output Shape
+        ## TODO: Duplicate Detection
+
+        # Cleanup
+        os.remove('../web_scraper/test.csv')
+        os.remove('../web_scraper/test_log.txt')
+        output = subprocess.check_output(tbl_drop_cmd, cwd=target_dir, shell=True)
 
 
 class TestModelBuild(unittest.TestCase):
     def test_run(self):
         """
-        Basic Completion
+        Basic Functional Test
         :return:
         """
         pass
 
-    def test_expected_metrics(self):
-        """
-        Sample Metrics ~ Expected Metrics
-        :return:
-        """
-        pass
+    # def test_expected_metrics(self):
+    #     """
+    #     Sample Metrics ~ Expected Metrics
+    #     :return:
+    #     """
+    #     pass
 
 
 class TestWebServer(unittest.TestCase):
     def test_run(self):
         """
-        Basic Completion
+        Basic Functional Test
         :return:
         """
         pass
 
-    def test_ui(self):
-        """
-        Basic UI Functionality
-        :return:
-        """
-        pass
+    # def test_ui(self):
+    #     """
+    #     Basic UI Functionality
+    #     :return:
+    #     """
+    #     pass
 
 
 if __name__ == '__main__':
